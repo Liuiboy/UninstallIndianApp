26c30df7975492e54fdd38da279e12796be9047b
六宅 <498431454@qq.com>
2020-09-02 15:36:39 +0800

    完善app

--- app/src/main/java/com/example/log/MainActivity.java
+++ app/src/main/java/com/example/log/MainActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.example.log.ui.main.MainFragment;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout startScan;
     private RelativeLayout inScan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
                     .commitNow();
         }
 
-       // findViewById(R.id.in_scan).setVisibility(View.INVISIBLE);
+       // findViewById(R.id.in
+        // _scan).setVisibility(View.INVISIBLE);
         //findViewById(R.id.start_scan).setVisibility(View.VISIBLE)
 
     }
@@ -36,14 +39,26 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-
                 try {
-                    Thread.sleep(5000);
-                    findViewById(R.id.in_scan).setVisibility(View.INVISIBLE);
+                    Thread.sleep(1500);
+                    final ImageView imageView=findViewById(R.id.log);
+
+                    imageView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageResource(R.drawable.duig);
+                            findViewById(R.id.in_scan).setVisibility(View.INVISIBLE);
+                            View rView = findViewById(R.id.ring);
+                            rView.setPadding(30,30,30,30);
+                            rView.setBackgroundResource(R.drawable.fillet_2);
+                            findViewById(R.id.complete_scan).setVisibility(View.VISIBLE);
+                        }
+                    });
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        });
+        }).start();
     }
 }
